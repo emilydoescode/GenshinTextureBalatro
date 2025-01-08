c07f46991f52a03279b55d63716f317870b53a9d
Emily <muffintimeyay@gmail.com>
2025-01-08 20:31:33 -0300

    as

---
+++ Genshin.lua
@@ -0,0 +1,11 @@
+----------------------------------------------
+------------MOD CODE -------------------------
+
+
+sendDebugMessage("Launching Negate Texture Pack!", "NegateTexturePack")
+
+SMODS.Atlas{key = "cards_1", path = "8BitDeck.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas{key = "cards_2", path = "8BitDeckopt2.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Language{key = "Genshinglish", label = "Genshinglish", path = "genshinglish.lua", font = 1}
+----------------------------------------------
+------------MOD CODE END----------------------
